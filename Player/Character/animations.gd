f2d65860db7047bc97ab589448d54aa715d35f8f
sugo <fuyuhimematsuri@gmail.com>
2023-02-17 15:28:22 +0100

    Moved to Client folder

--- Player/Character/animations.gd
+++
@@ -1,5 +0,0 @@
-extends Node3D
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$Skeleton3D/Head.rotation = Vector3(1000, 1000, 0)
--- Player/Character/character_rogue/spawnerScript.gd
+++
@@ -1,6 +0,0 @@
-extends Button
-
-var source = preload("./character.tscn")
-
-func _on_pressed():
-	$"../.."._add_character(source)
