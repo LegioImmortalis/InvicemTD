da305a95c0db8020d627676c22a81d07c7938a0d
sugo <fuyuhimematsuri@gmail.com>
2023-02-17 15:27:26 +0100

    Placing works, but no collision

--- Buildings.gd
+++ Buildings.gd
@@ -1,30 +1,12 @@
 extends Node3D
-
-@onready var baracks= preload("res://baracks.tscn")
-@onready var tower 	= preload("res://tower.tscn")
-@onready var smithy = preload("res://smithy.tscn")
 # Called when the node enters the scene tree for the first time.
 
-var instance
+@onready var Castle = preload("res://castle.tscn")
 
 func _ready():
+	add_child(Castle.instantiate())
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
-
-func place(pos):
-	if instance:
-		$"../Buildings".instance.transform.origin = pos
-	
-func _input(event):
-	if instance:
-		if event is InputEventMouseButton:
-			$"../Buildings".instance.get_node("Mesh").get_active_material(3).albedo_color *= Color(1, 1, 1, 1)
-			$"../Buildings".instance = null
-
-func build(building):
-	if building == "tower":
-		instance = tower.instantiate()
-		instance.get_node("Mesh").get_active_material(3).albedo_color *= Color(1, 1, 1, 0.5)
--- character.gd
+++ character.gd
@@ -3,12 +3,12 @@ extends CharacterBody3D
 @onready var anim_player = $Animations/AnimationPlayer
 @onready var raycast = $FPS/RayCast3D
 #@onready var server = get_node("/root/World")
-@onready var camera = $"../TOP"
 @onready var Baracks= preload("res://baracks.tscn")
 @onready var Tower 	= preload("res://tower.tscn")
 @onready var Armory = preload("res://armory.tscn")
 @onready var Market = preload("res://house.tscn")
 @onready var House = preload("res://market.tscn")
+@onready var star = preload("res://star.tscn")
 
 const SPEED = 50
 const JUMP_VELOCITY = 10.0
@@ -19,15 +19,19 @@ var distance
 var gravity = 20.0
 var cameraMode = 0
 var instance
-
+var mousePos 
+var Star
+var camera
 
 func _ready():
+	instance = star.instantiate()
 	set_multiplayer_authority(str(name).to_int())	
-	$"../TOP".current = true
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	camera = $TOP
 	pass
 
 func _unhandled_input(event):
-	var mousePos = camera.get_viewport().get_mouse_position()
+	mousePos = camera.get_viewport().get_mouse_position()
 	var rayLength = 200
 	var space = get_world_3d().direct_space_state
 	var rayQuery = PhysicsRayQueryParameters3D.new()
@@ -36,38 +40,44 @@ func _unhandled_input(event):
 	rayQuery.collide_with_areas = false
 	rayQuery.collide_with_bodies = true
 	var result = space.intersect_ray(rayQuery)
+	
 	if result:
 		destination = result.position
+		print (result.collider)
 	
 	if event is InputEventMouseMotion:
-		
-		#var scr = get_viewport().get_visible_rect().size
-		#rota=Vector2((event.global_position.x-scr.x/2),(event.global_position.y-scr.y/2))
-		#rotaNormal=Vector2((event.global_position.x-scr.x/2)/scr.x,(event.global_position.y-scr.y/2)/scr.y)
-		#distance=Vector2(0,0).distance_to(Vector2((event.global_position.x-scr.x/2)/scr.x,(event.global_position.y-scr.y/2)/scr.y))
 
 		if cameraMode != 2:
 			rotate_y(-event.relative.x * .005)
 			camera.rotate_x(-event.relative.y * .005)
 			camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 			
 		if cameraMode == 2:
+			Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
 			if destination:
 				look_at(Vector3(destination.x,1,destination.z))
 			#camera.rotation = Vector3(deg_to_rad(-90),deg_to_rad(0),deg_to_rad(0))
-		
-			if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-				print (result.position)
-				pass
 			
 		if instance:
-			instance.position = round(destination*Vector3(1,0,1))
-			
-	if instance:
+			instance.position = destination*Vector3(1,0,1)
+	if instance && instance != Star:
+		instance.position = round(destination*Vector3(1,0,1))
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			#instance.get_node("Mesh").get_active_material().albedo_color *= Color(1, 1, 1, 1)
-			instance = Node3D.new()
-	
+			#instance.get_node("Collision").disabled = false
+			instance = Star
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+			instance.free()
+			instance = Star
+		if Input.is_action_just_pressed("q"):	
+			print(instance.rotation)
+			instance.rotation.y += deg_to_rad(90)
+			print(instance.rotation)
+		if Input.is_action_just_pressed("e"):	
+			print(instance.rotation)
+			instance.rotation.y -= deg_to_rad(90)
+			print(instance.rotation)
 	#if Input.is_action_just_pressed("") \
 	#		and anim_player.current_animation != "":
 	#	play_effects("shoot")
@@ -79,6 +89,9 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
+	if cameraMode == 2:
+		camera.position=Vector3(position.x,camera.position.y,position.z)
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim_player.play("KayKit Animated Character|Hop")
@@ -87,14 +100,11 @@ func _physics_process(delta):
 	var direction
 	
 	if cameraMode != 2:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		if input_dir:
 			direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if cameraMode == 2:
-		#camera.rotation = Vector3(deg_to_rad(-70),deg_to_rad(0),deg_to_rad(0))
-		camera.position = position + Vector3 (0,150,55)	
-		Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+		#camera.rotation = Vector3(deg_to_rad(-70),deg_to_rad(0),deg_to_rad(0)
 		if input_dir:
 			direction = (Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
@@ -136,13 +146,15 @@ func _unhandled_key_input(event):
 			camera=$TPS
 			$TPS.current = true
 		if cameraMode == 2: 
-			camera=$"../TOP"
-			$"../TOP".current = true
+			camera=$TOP
+			$TOP.current = true
 
 
 # Called when the node enters the scene tree for the first time.
 	
 func build(building):
+	if instance:
+		instance.free()
 	if building == "Demolish":
 		#instance = Demolish.instantiate()
 		pass
